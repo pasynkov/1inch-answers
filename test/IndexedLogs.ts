bab0889a966cf8fdabaf2aa255ee02c30580c7db
pasynkov <n@pasynkov.com>
2023-05-25 10:29:12 +0300

    add indexed logs

--- test/IndexedLogs.ts
+++ test/IndexedLogs.ts
@@ -3,6 +3,7 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { BigNumber, BytesLike } from 'ethers';
+import { ContractTransaction } from '@ethersproject/contracts/src.ts';
 
 describe("IndexedLogs", function () {
   // We define a fixture to reuse the same setup in every test.
@@ -46,22 +47,30 @@ describe("IndexedLogs", function () {
 
     });
 
-    it("Checks indexed logs", async function () {
-      const { indexedLogs, owner } = await loadFixture(deployOneYearLockFixture);
+    it("Filter indexed logs", async function () {
+      const { indexedLogs, owner, otherAccount } = await loadFixture(deployOneYearLockFixture);
+
       const randomNum = Math.floor(Math.random() * 200 + 1);
+      const [
+        indexedEventAddress,
+        nonIndexedEventAddress,
+      ] = await Promise.all([
+          new Promise(resolve => {
+            indexedLogs.on(indexedLogs.filters.IndexedAction(owner.address), (address: string, num: BigNumber) => {
+              resolve(address);
+            });
+          }),
+          new Promise(resolve => {
+            indexedLogs.on(indexedLogs.filters.NonIndexedAction(), (address: string, num: BigNumber) => {
+              resolve(address);
+            });
+          }),
+          indexedLogs.connect(otherAccount).doAction(randomNum).then((t: ContractTransaction) => t.wait()),
+          indexedLogs.connect(owner).doAction(randomNum).then((t: ContractTransaction) => t.wait()),
+      ]);
 
-      const result = await (await indexedLogs.doAction(randomNum)).wait();
-
-      await expect(result.events).is.not.empty;
-      const event = result.events?.find((e: { event: string; }) => e.event === 'IndexedAction');
-      console.log('messageEvent', event);
-      await expect(event).is.not.empty;
-      await expect(event!.args).is.not.empty;
-      const { caller, actionNum } = event!.args as unknown as { caller: string, actionNum: BigNumber };
-      expect(actionNum.toNumber()).to.equal(randomNum);
-      expect(caller).to.equal(owner.address);
-
-      indexedLogs.on
+      expect(indexedEventAddress).to.equal(owner.address);
+      expect(nonIndexedEventAddress).to.equal(otherAccount.address);
 
     });
   });
